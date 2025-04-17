b06ad5486cb4ab097b815410368062142275c0cd
AbdullahSholi <groupgroup060@gmail.com>
2025-04-17 22:22:00 +0300

    Create database tables

---
+++ CreateDatabaseTables.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Borrowers (
+	BorrowerID INT PRIMARY KEY IDENTITY(1,1),
+	FirstName VARCHAR(50) NOT NULL,
+	LastName VARCHAR(50) NOT NULL,
+	Email VARCHAR(50) NOT NULL,
+	DateOfBirth DATE NOT NULL,
+	MembershipDate Date NOT NULL
+);
+
+CREATE TABLE Books (
+	BookID INT PRIMARY KEY IDENTITY(1,1),
+	Title VARCHAR(50) NOT NULL,
+	Author VARCHAR(50) NOT NULL,
+	ISBN VARCHAR(17) NOT NULL,
+	PublishedData DATE NOT NULL,
+	Genre VARCHAR(50) NOT NULL,
+	ShelfLocation VARCHAR(50) NOT NULL,
+	CurrentStatus VARCHAR(50) NOT NULL CHECK (CurrentStatus IN('Available', 'Borrowed'))
+);
+
+CREATE TABLE Loans (
+	LoanID INT PRIMARY KEY IDENTITY(1,1),
+	BookID INT NOT NULL,
+	BorrowerID INT NOT NULL,
+	DataBorrowed DATE NOT NULL,
+	DueDate DATE NOT NULL,
+	DateReturned DATE NOT NULL,
+	FOREIGN KEY(BookID) REFERENCES Books(BookID),
+	FOREIGN KEY(BorrowerID) REFERENCES Borrowers(BorrowerID)
+);
