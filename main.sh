2923c003aef883866cf79ccc9a782f496a926a20
AbdullahGSholi <groupgroup060@gmail.com>
2025-04-19 13:53:28 +0300

    Temp table for list overdue borrowers

--- main.sh
+++ main.sh
@@ -38,6 +38,8 @@ $BASE_SQLCMD -i 11.storedProcedure-BorrowedBooksReport.sql
 
 $BASE_SQLCMD -i 12.triggerImplementation.sql
 
+$BASE_SQLCMD -i 13.tempTable.sql
+
 $BASE_SQLCMD -i DeleteTablesData.sql
 
 
